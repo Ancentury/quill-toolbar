68413f68ce199e0ea7e098bf3b6dc2879caa3d13
Qing An <qingan88@hotmail.com>
2019-11-11 23:35:37 -0500

    added image compress

--- src/quill_custom.js
+++ src/quill_custom.js
@@ -1,34 +1,38 @@
 Quill.register("modules/imageCompressor", imageCompressor);
-        const toolbarOption = [
-            ['bold', 'italic'],
-            ['link', 'image'],
-            [{ 'header': [1, 2, 3, 4, false] }],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            // [{ size: ['small', false, 'large', 'huge'] }],
-            // ['blockquote', 'code-block'],
-            // [{ 'script': 'sub' }, { 'script': 'super' }],
-            // [{ 'indent': '-1' }, { 'indent': '+1' }],
-            // [{ 'direction': 'rtl' }],
-            // [{ 'color': [] }, { 'background': [] }],
-            // [{ 'font': [] }],
-            // [{ 'align': [] }],
-            // //    [ 'undo','redo'],
-            // ['clean']
-        ];
+const toolbarOption = [
+    ['bold', 'italic'],
+    ['link', 'image'],
+    [{ 'header': [1, 2, 3, 4, false] }],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ size: ['small', false, 'large', 'huge'] }],
+    ['blockquote', 'code-block'],
+    [{ 'script': 'sub' }, { 'script': 'super' }],
+    [{ 'indent': '-1' }, { 'indent': '+1' }],
+    [{ 'direction': 'rtl' }],
+    [{ 'color': [] }, { 'background': [] }],
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    //    [ 'undo','redo'],
+    ['clean']
+];
 
-        console.log('Demo loaded...')
+console.log('Demo loaded...')
 
-        var quill = new Quill('#editor', {
+var quill = new Quill('#editor', {
 
-            theme: "snow",
-            modules: {
-                toolbar: toolbarOption,
-                imageCompressor: {
-                    quality: 0.7,
-                    maxWidth: 1000,
-                    imageType: 'image/jpeg',
-                    //debug: true,
-                }
-                //magicUrl: true
-            }
-        });
+    theme: "snow",
+    modules: {
+        toolbar: toolbarOption,
+        imageCompressor: {
+            quality: 0.7,
+            maxWidth: 500,
+            maxHeight: 500,
+            imageType: 'image/jpeg',
+            debug: true,
+        },
+        magicUrl: {
+            globalRegularExpression: /(https?:\/\/|www\.|mailto:)[\S]+/g,
+            urlRegularExpression: /(https?:\/\/[\S]+)|(www.[\S]+)|(mailto:[\S]+)/
+        }
+    }
+});
